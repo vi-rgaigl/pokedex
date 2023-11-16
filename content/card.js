b73288cf7aaaaf387eb337d9524abcce7ea3c8cf
Regina Gaigl <regina.gaigl@gmail.com>
2023-11-16 19:44:21 +0100

    initial commit

---
+++ content/card.js
@@ -0,0 +1,3 @@
+function generatePokemonInfoBodyHTML() {
+    return 
+}
---
+++ script.js
@@ -0,0 +1,57 @@
+let currentPokemon;
+
+async function init() {
+    await loadPokemon();
+}
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let resopnse = await fetch(url);
+    currentPokemon = await resopnse.json();
+    renderPokemonCard();
+}
+
+function renderPokemonCard() {
+    document.getElementById('pokemon_name').innerHTML = capitalizeFirstLetter(currentPokemon['name']);
+    document.getElementById('pokemon_image').src = currentPokemon['sprites']['other']['home']['front_default'];
+    document.getElementById('pokemon_id').innerHTML = formatId(currentPokemon['id']);
+}
+
+function renderPokemonInfos() {
+    generatePokemonInfoBodyHTML();
+}
+
+
+function openInfoSection(tab, section) {
+    let infoDivs = document.getElementsByClassName('info');
+    for (let i = 0; i < infoDivs.length; i++) {
+        infoDivs[i].style.display = 'none';
+    }
+    for (let i = 1; i <= 4; i++) {
+        setBorderMarker(i, tab);
+    }
+    document.getElementById(section).style.display = 'block';
+}
+
+function setBorderMarker(i, tab) {
+    document.getElementById(`tab_${tab}`).classList.remove('w3-bottombar');
+    document.getElementById(`tab_${tab}`).classList.add('border-marker');
+    if (tab != i) {
+        document.getElementById(`tab_${i}`).classList.add('w3-bottombar');
+        document.getElementById(`tab_${i}`).classList.remove('border-marker');
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function formatId(id) {
+    if (id < 10) {
+        return '#00' + id;
+    } else if (id < 100 && id > 10) {
+        return '#0' + id;
+    } else {
+        return '#' + id;
+    }
+}
